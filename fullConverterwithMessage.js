ef465492db303e46247dfff0f437d1e29fae5b27
EvaDM <evamorante1969@gmail.com>
2023-04-09 00:07:14 +0200

    creating code for the 21. exercice

---
+++ fullConverterwithMessage.js
@@ -0,0 +1,19 @@
+function changeMoney(dollars){
+    var francs = dollars*1.02;
+    var message = dollars+" dollars are "+francs+" francs.";
+    return message;
+}
+console.log(changeMoney(25));
+
+function changeWater(liters){
+    var gallons = liters*0.17;
+    var message = liters+" liters are"+gallons+" gallons.";
+    return message;
+}
+console.log(changeWater(3));
+
+function changeHeigth(meters){
+    var centimeters = meters/100;
+    var message = centimeters+" centimeters are"+meters+" meters."
+}
+console.log(changeHeigth);
